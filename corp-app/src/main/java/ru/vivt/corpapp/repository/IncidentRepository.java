879c0f479d4be044cd48c6ad3eb3686a657657c4
screen190795 <horizonjournal78@gmail.com>
2021-11-11 10:25:08 +0300

    Обновление методов репозитория

--- corp-app/src/main/java/ru/vivt/corpapp/repository/IncidentRepository.java
+++ corp-app/src/main/java/ru/vivt/corpapp/repository/IncidentRepository.java
@@ -1,7 +1,23 @@
 package ru.vivt.corpapp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import ru.vivt.corpapp.entity.Incident;
 
-public interface IncidentRepository extends JpaRepository<Incident, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface IncidentRepository extends CrudRepository<Incident, Long>, JpaSpecificationExecutor<Incident> {
+
+    Optional<Incident> findById(Long id);
+
+    List<Incident> findAll();
+
+
+    Page<Incident> findAll(Pageable page);
+
+
 }
