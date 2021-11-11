704a24f1d8326bdd312f7715eb5e1a4f3529020e
screen190795 <horizonjournal78@gmail.com>
2021-11-11 10:18:11 +0300

    Удаление метода manageIncidents()

--- corp-app/src/main/java/ru/vivt/corpapp/controller/HomeController.java
+++ corp-app/src/main/java/ru/vivt/corpapp/controller/HomeController.java
@@ -39,12 +39,6 @@ public class HomeController {
         return "staffMain";
     }
 
-    @GetMapping("/staff/incidents")
-    public String manageIncidents(@AuthenticationPrincipal MyUserDetails user, Model model) {
-        model.addAttribute("user", user.getUsername());
-        return "incidents";
-    }
-
     @GetMapping("/")
     public String index() {
         return "index";
