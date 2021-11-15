f9daf5572361c5c16f787dd2e068528e7b7ff0f5
screen190795 <horizonjournal78@gmail.com>
2021-11-15 10:22:34 +0300

    Добавление displayName

--- corp-app/src/main/java/ru/vivt/corpapp/entity/Priority.java
+++ corp-app/src/main/java/ru/vivt/corpapp/entity/Priority.java
@@ -1,9 +1,22 @@
 package ru.vivt.corpapp.entity;
 
 public enum Priority {
-    HIGEST,
-    HIGH,
-    MEDIUM,
-    LOW,
-    LOWEST
+    HIGEST("Блокирующий"),
+    HIGH("Высокий"),
+    MEDIUM("Средний"),
+    LOW("Низкий");
+
+    private String displayName;
+
+    Priority(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
--- corp-app/src/main/java/ru/vivt/corpapp/entity/Status.java
+++ corp-app/src/main/java/ru/vivt/corpapp/entity/Status.java
@@ -1,8 +1,22 @@
 package ru.vivt.corpapp.entity;
 
 public enum Status {
-    OPEN,
-    IN_PROGRESS,
-    DONE,
-    CANCELLED
+    OPEN("Открыт"),
+    IN_PROGRESS("В работе"),
+    DONE("Закрыт"),
+    CANCELLED("Отменен");
+
+    private String displayName;
+
+    Status(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
