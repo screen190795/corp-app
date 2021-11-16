c3fb3ae4a4c61c045442f4ebdd2eff654684b099
screen190795 <horizonjournal78@gmail.com>
2021-11-16 09:37:10 +0300

    Правка сущности репозитория, добавление метода findByUser()

--- corp-app/src/main/java/ru/vivt/corpapp/repository/StaffRepository.java
+++ corp-app/src/main/java/ru/vivt/corpapp/repository/StaffRepository.java
@@ -1,7 +1,14 @@
 package ru.vivt.corpapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import ru.vivt.corpapp.entity.Reporter;
 import ru.vivt.corpapp.entity.Staff;
+import ru.vivt.corpapp.entity.User;
 
-public interface StaffRepository extends JpaRepository<Staff, Integer> {
+import java.util.List;
+
+public interface StaffRepository extends CrudRepository<Staff, Long>, JpaSpecificationExecutor<Staff> {
+    List<Staff> findByUser(User user);
 }
